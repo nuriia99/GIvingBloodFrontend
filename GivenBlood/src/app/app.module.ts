54ec4d60634ac03199aef5c00796a16cf1045d80
Nuria Antón Calle <nurianton1999@gmail.com>
2023-05-15 12:42:05 +0200

    implementation input search done

--- GivenBlood/src/app/app.module.ts
+++ GivenBlood/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- GivenBlood/src/app/components/want-give/want-give.component.ts
+++ GivenBlood/src/app/components/want-give/want-give.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+
+interface Hospital {
+  id:number,
+  address:string
+}
 
 @Component({
   selector: 'app-want-give',
   templateUrl: './want-give.component.html',
   styleUrls: ['./want-give.component.css']
 })
-export class WantGiveComponent {
 
+export class WantGiveComponent {
+  open:Boolean = false;
+  searchValue:any;
+  centers: Hospital[] = [
+    { id: 12, address: 'Dr. Nice' },
+    { id: 13, address: 'Bombasto' },
+    { id: 14, address: 'Celeritas' },
+    { id: 15, address: 'Magneta' },
+    { id: 16, address: 'RubberMan' },
+    { id: 17, address: 'Dynama' },
+    { id: 18, address: 'Dr. IQ' },
+    { id: 19, address: 'Magma' },
+    { id: 20, address: 'Tornado' }
+  ];
+
+
+  updateSearchValue() {
+    if(this.searchValue.length > 0) this.open = true;
+    else this.open = false;
+  }
 }
